10e25b782dd8b38daf0edd5f911faf4a8fb6e3b6
Jure Cular <jure.cular@gmail.com>
2020-04-22 20:14:49 +0200

    Finish up RepositoryDetailsViewController

--- GtihubSearch/GithubSearch/Modules/Details/RepositoryDetailsViewController.swift
+++ GtihubSearch/GithubSearch/Modules/Details/RepositoryDetailsViewController.swift
@@ -22,22 +22,133 @@ extension Repository: RepositoryDetails {
 }
 
 final class RepositoryDetailsViewController: UIViewController {
-
+    private let _repositoryDetails: RepositoryDetails
+    
+    init(repositoryDetails: RepositoryDetails) {
+        _repositoryDetails = repositoryDetails
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor.systemBackground
+        
+        title = _repositoryDetails.repositoryName
+        
+        let scrollView = UIScrollView()
+        _setup(scrollView: scrollView)
+        
+        let stackView = UIStackView()
+        _setup(stackView: stackView, inScrollView: scrollView)
+        
+        let repositoryDetailsLabelText = NSLocalizedString("repositoryDetailsName", comment: "")
+        
+        let repositoryNameDetailView = RepositoryDetailView(labelText: "\(repositoryDetailsLabelText):", detailText: _repositoryDetails.repositoryName, font: UIFont.systemFont(ofSize: 20))
+        _setupDetailLabel(repositoryDetailView: repositoryNameDetailView, inView: stackView)
+        
+        let repositoryDescriptionLabelText = NSLocalizedString("repositoryDetailsDescription", comment: "")
+        let repositoryDescriptionDetailView = RepositoryDetailView(labelText: "\(repositoryDescriptionLabelText):", detailText: _repositoryDetails.description, font: UIFont.systemFont(ofSize: 18))
+        _setupDetailLabel(repositoryDetailView: repositoryDescriptionDetailView, inView: stackView)
+        
+        let repositoryOwnerNameLabelText = NSLocalizedString("repositoryDetailsOwnerName", comment: "")
+        let repositoryOwnerNameDetailView = RepositoryDetailView(labelText: "\(repositoryOwnerNameLabelText):", detailText: _repositoryDetails.ownerName, font: UIFont.systemFont(ofSize: 17))
+        _setupDetailLabel(repositoryDetailView: repositoryOwnerNameDetailView, inView: stackView)
+        
+        let repositoryUpdateTimeLabelText = NSLocalizedString("repositoryDetailsUpdateTime", comment: "")
+        let lastUpdateTimeDetailView = RepositoryDetailView(labelText: "\(repositoryUpdateTimeLabelText):", detailText: _repositoryDetails.lastUpdateDate, font: UIFont.systemFont(ofSize: 15))
+        _setupDetailLabel(repositoryDetailView: lastUpdateTimeDetailView, inView: stackView)
+        
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Setup -
+    private func _setup(stackView: UIStackView, inScrollView scrollView: UIScrollView) {
+        let spacingConstant: CGFloat = 10
+        stackView.alignment = .fill
+        stackView.distribution = .fill
+        stackView.axis = .vertical
+        stackView.spacing = spacingConstant
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        scrollView.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.widthAnchor.constraint(equalTo: view.widthAnchor),
+            stackView.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor)
+        ])
     }
-    */
+    
+    private func _setup(scrollView: UIScrollView) {
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(scrollView)
+        
+        NSLayoutConstraint.activate([
+            scrollView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+    
+    private func _setupDetailLabel(repositoryDetailView: RepositoryDetailView, inView stackView: UIStackView) {
+        repositoryDetailView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.addArrangedSubview(repositoryDetailView)
+        
+        NSLayoutConstraint.activate([
+            repositoryDetailView.widthAnchor.constraint(equalTo: view.widthAnchor)
+        ])
+    }
+}
 
+fileprivate class RepositoryDetailView: UIView {
+    
+    init(labelText: String, detailText: String?, font: UIFont = UIFont.systemFont(ofSize: 17)) {
+        super.init(frame: .zero)
+        
+        let spacingConstant: CGFloat = 3
+        
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.distribution = .fill
+        stackView.alignment = .top
+        stackView.spacing = spacingConstant
+        
+        let descriptionLabel = UILabel()
+        descriptionLabel.textColor = UIColor.lightGray
+        descriptionLabel.text = labelText
+        descriptionLabel.font = font
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        stackView.addArrangedSubview(descriptionLabel)
+        
+        let detailLabel = UILabel()
+        detailLabel.lineBreakMode = .byWordWrapping
+        detailLabel.numberOfLines = 0
+        detailLabel.text = detailText
+        detailLabel.font = font
+        detailLabel.translatesAutoresizingMaskIntoConstraints = false
+        stackView.addArrangedSubview(detailLabel)
+        
+        descriptionLabel.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+        descriptionLabel.setContentCompressionResistancePriority(.defaultHigh, for: .horizontal)
+        
+        addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            stackView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            stackView.topAnchor.constraint(equalTo: topAnchor),
+            stackView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
