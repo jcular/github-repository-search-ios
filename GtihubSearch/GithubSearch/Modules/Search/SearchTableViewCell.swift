6c174ff2904732b1a7b5d71165ba1fe0c792d6cd
Jure Cular <jure.cular@gmail.com>
2020-04-22 20:29:32 +0200

    Finish SearchTableViewCell

--- GtihubSearch/GithubSearch/Modules/Search/SearchTableViewCell.swift
+++ GtihubSearch/GithubSearch/Modules/Search/SearchTableViewCell.swift
@@ -14,9 +14,20 @@ protocol SearchTableViewCellItem {
 }
 
 final class SearchTableViewCell: UITableViewCell {
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     func configure(item: SearchTableViewCellItem) {
-        self.textLabel?.text = item.repositoryName
-        self.detailTextLabel?.text = item.lastUpdateDate
+        accessoryType = .disclosureIndicator
+        textLabel?.text = item.repositoryName
+        detailTextLabel?.text = item.lastUpdateDate
     }
 }
 
