6d8985fc336030f514d1aa8a5463dfaa6125746a
Jure Cular <jure.cular@gmail.com>
2020-04-22 16:18:18 +0200

    Set root view controller to SearchViewController in AppDelegate

--- GtihubSearch/GtihubSearch/SupportingFiles/AppDelegate.swift
+++ GtihubSearch/GtihubSearch/SupportingFiles/AppDelegate.swift
@@ -10,16 +10,20 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
+    
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        return true
-    }
+        let initialController = UINavigationController()
+        initialController.setRoot(viewRouter: SearchViewRouter(), animated: false)
 
         self.window = UIWindow(frame: UIScreen.main.bounds)
 
+        self.window?.rootViewController = initialController
+        self.window?.makeKeyAndVisible()
+        
+        return true
     }
 
 }
